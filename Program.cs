a327fca6c782b953545fa0721aee4a43ecce918c
John Rowley <outsidethebox@eircom.net>
2021-11-09 17:32:30 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using AbstractSuperHeroWithInterface01.Models;
+using System;
+
+namespace AbstractSuperHeroWithInterface01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            // SuperHero superHero = new();
+
+            SuperMan superMan = new SuperMan();
+            superMan.Fly();
+            superMan.LookThroughObjects();
+
+            Sword sword = new Sword("BatSword");
+            sword.TakeDamage(5);
+        }
+    }
+}
